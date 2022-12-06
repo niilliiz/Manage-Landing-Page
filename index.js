60959a6d51a62a6428a2c2c831d86e3ce842c411
niilliiz <niloo.vatan.nv@gmail.com>
2022-12-06 13:42:20 +0330

    feat: menu button

---
+++ index.js
@@ -0,0 +1,10 @@
+const menuBtn = document.querySelector("#menu-btn");
+const menu = document.querySelector("#menu");
+
+menuBtn.addEventListener("click", openMenuHandler);
+
+function openMenuHandler() {
+  menuBtn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
